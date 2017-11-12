9706af4e1e8d0c0348b6740f94e37151127f513a
Kevin <7211424+Xmaxer@users.noreply.github.com>
2017-11-12 21:54:30 +0000

    Can now search the database from input

--- src/Downloader.java
+++ src/Downloader.java
@@ -243,29 +243,6 @@ public class Downloader {
 		return classes;
 	}
 
-/*	private static String getDayFromNumber(int number) {
-		
-		switch(number)
-		{
-		case 1:
-			return "Monday";
-		case 2:
-			return "Tuesday";
-		case 3:
-			return "Wednesday";
-		case 4:
-			return "Thursday";
-		case 5:
-			return "Friday";
-		case 6:
-			return "Saturday";
-		case 7:
-			return "Sunday";
-			default:
-				return null;
-		}
-	}*/
-
 	private static Document downloadPage(String link) {
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -18,36 +21,106 @@ public class Main extends Application{
 		
 		//Downloader.redownloadData();
 		
-        String sql = "SELECT * FROM room";
-        
-        try (
-
-             ResultSet rs    = DBConnection.getStatement().executeQuery(sql)){
-            
-            // loop through the result set
-            while (rs.next()) {
-                System.out.println(rs.getString("room_number") +  "\t" + 
-                                   rs.getInt("capacity") + "\t");
-            }
-            
-            sql = "SELECT * FROM class";
-            ResultSet rs2 = DBConnection.getStatement().executeQuery(sql);
-            
-            while (rs2.next()) {
-            	System.out.println("-----------------------");
-                System.out.println(rs2.getString("time") +  "\t" + 
-                                   rs2.getString("day") + "\t" +
-                		rs2.getString("module") + "\t" +
-                                   rs2.getInt("week_number") + "\t" + 
-                                   rs2.getString("room_number") + "\t" + 
-                                   rs2.getString("class_group") + "\t" +
-                                   rs2.getInt("length"));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+		LocalDateTime now = LocalDateTime.now();
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Input date: ");
+		String day = input.nextLine();
+		System.out.print("Input time: ");
+		String time = input.next();
 		
 
+		Calendar cal = Calendar.getInstance();
+		if(cal.get(Calendar.DAY_OF_MONTH) <= Integer.valueOf(day))
+		{
+			cal.set(Calendar.DAY_OF_MONTH, Integer.valueOf(day));
+			day = getDayFromNumber(cal.get(Calendar.DAY_OF_WEEK));
+			getRoomUsage(day, time);
+		}
+		
+		
+		//System.out.println(now);
+		
+		//printAll();
+
+	}
+
+private static String getDayFromNumber(int number) {
+		
+		switch(number)
+		{
+		case 2:
+			return "Monday";
+		case 3:
+			return "Tuesday";
+		case 4:
+			return "Wednesday";
+		case 5:
+			return "Thursday";
+		case 6:
+			return "Friday";
+		case 0:
+			return "Saturday";
+		case 1:
+			return "Sunday";
+			default:
+				return null;
+		}
+	}
+
+	private static void getRoomUsage(String day, String time) {
+		//TO DO implement smart system that detects things like 16:30
+		try {
+        String sql = "SELECT * FROM class WHERE day='" + day + "' AND time='" + time + "' COLLATE NOCASE";
+        ResultSet rs = DBConnection.getStatement().executeQuery(sql);
+        
+        while(rs.next()) {
+        	System.out.println("-----------------------");
+            System.out.println(rs.getString("time") +  "\t" + 
+                               rs.getString("day") + "\t" +
+            		rs.getString("module") + "\t" +
+                               rs.getInt("week_number") + "\t" + 
+                               rs.getString("room_number") + "\t" + 
+                               rs.getString("class_group") + "\t" +
+                               rs.getInt("length"));
+        }
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void printAll() {
+		 String sql = "SELECT * FROM room";
+	        
+	        try (
+
+	             ResultSet rs    = DBConnection.getStatement().executeQuery(sql)){
+	            
+	            // loop through the result set
+	            while (rs.next()) {
+	                System.out.println(rs.getString("room_number") +  "\t" + 
+	                                   rs.getInt("capacity") + "\t");
+	            }
+	            
+	            sql = "SELECT * FROM class";
+	            ResultSet rs2 = DBConnection.getStatement().executeQuery(sql);
+	            
+	            while (rs2.next()) {
+	            	System.out.println("-----------------------");
+	                System.out.println(rs2.getString("time") +  "\t" + 
+	                                   rs2.getString("day") + "\t" +
+	                		rs2.getString("module") + "\t" +
+	                                   rs2.getInt("week_number") + "\t" + 
+	                                   rs2.getString("room_number") + "\t" + 
+	                                   rs2.getString("class_group") + "\t" +
+	                                   rs2.getInt("length"));
+	            }
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        }
+			
 	}
 
 	@Override
